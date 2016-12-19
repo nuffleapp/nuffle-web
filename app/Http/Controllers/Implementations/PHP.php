6584b3f871d7be3a94c1f3548234fbfaf10600a7
Zachary Flower <zach@ninjaninja.net>
2016-12-19 16:33:29 -0700

    first commit

---
+++ app/Http/Controllers/Implementations/PHP.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Implementations;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PHP extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Integrations/Slack.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Integrations;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Slack extends Controller {
+  public function index() {
+
+  }
+
+  public function auth() {
+
+  }
+}
